d81bacf21201ceeda45589e3badd52556903a79b
Matthew Crist <matthew.crist@boston.gov>
2016-12-13 13:10:44 -0500

    Add type to search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,27 @@ module ApplicationHelper
     text.chomp("| City of Boston").chomp("| Boston.gov")
   end
 
+  def type(type)
+    case type
+    when "topic_page"
+      return "Topic"
+    when "department_profile"
+      return "Department"
+    when "article"
+      return "Article"
+    when "event"
+      return "Event"
+    when "post"
+      return "News"
+    when "how_to"
+      return "How To"
+    when "program_initiative_profile"
+      return "Program / Initiative"
+    else
+      return type
+    end
+  end
+
   def pagination_url(params)
     request.base_url + '?' + params.to_query
   end
